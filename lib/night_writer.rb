4537c01e066fbc2523304185691d9d5d53dd2156
Matt Deming <vballmatt44@gmail.com>
2022-04-25 14:31:31 -0600

    feat: Add alphabet and translate methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,17 +1,30 @@
 require 'pry'
+require './lib/translator'
+
+include Translator
+
 file = File.open(ARGV[0], "r")
-puts "Created '#{ARGV[1]}' containing #{file.read.length} characters"
 
-p File.read(file
-
-File.write('./braille.txt', File.read(file))
-
-p File.read(ARGV[1])
-
-File.open(ARGV[0], 'rb') do |input_stream|
-  File.open(ARGV[1], 'wb') do |output_stream|
-    IO.copy_stream(input_stream, output_stream)
-    # binding.pry
+file2 = File.open(ARGV[1], "w")
+File.open(file, "r") do |file_object|
+  file_object.each_char do |char|
+    file2 << translate(char)
   end
 end
+binding.pry
+
+file2.close
+
+# File.write(ARGV[1], File.read(file))
+
+puts "Created '#{ARGV[1]}' containing #{file.read.length} characters"
+# p File.read(ARGV[1])
+file.close
+
+# File.open(ARGV[0], 'rb') do |input_stream|
+#   File.open(ARGV[1], 'wb') do |output_stream|
+#     IO.copy_stream(input_stream, output_stream)
+#     # binding.pry
+#   end
+# end
 #file.truncate(80)
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -4,12 +4,47 @@ SimpleCov.start
 require_relative '../lib/translator.rb'
 
 RSpec.describe Translator do
-  before :each do
-    translator = Translator.new
+  # before :each do
+  #   @translator = Translator.new
+  # end
+
+  # it 'exists' do
+  #   expect(@translator).to be_a(Translator)
+  # end
+
+  it "can show a hash for all the letters and their 'base' braille equivelent" do
+    expect(eng_braille_dict).to eq({
+      "a" => "0.\n..\n..",
+      "b" => "0.\n0.\n..",
+      "c" => "00\n..\n.",
+      "d" => "00\n.0\n..",
+      "e" => "0.\n.0\n..",
+      "f" => "00\n0.\n..",
+      "g" => "00\n00\n..",
+      "h" => "0.\n00\n..",
+      "i" => ".0\n0.\n..",
+      "j" => ".0\n00\n..",
+      "k" => "0.\n..\n0.",
+      "l" => "0.\n0.\n0.",
+      "m" => "00\n..\n0.",
+      "n" => "00\n.0\n0.",
+      "o" => "0.\n.0\n0.",
+      "p" => "00\n0.\n0.",
+      "q" => "00\n00\n0.",
+      "r" => "0.\n00\n0.",
+      "s" => ".0\n0.\n0.",
+      "t" => ".0\n00\n0.",
+      "u" => "0.\n..\n00",
+      "v" => "0.\n0.\n00",
+      "w" => ".0\n00\n.0",
+      "x" => "00\n..\n00",
+      "y" => "00\n.0\n00",
+      "z" => "0.\n.0\n00",
+      " " => "..\n..\n.."
+    })
   end
 
-  it 'exists' do
-    expect(translator).to be_a(Translator)
+  it 'can take the letter "a" and show its braille pattern' do
+    expect(@translator.translate("a")).to eq("0.\n..\n..")
   end
-
 end
